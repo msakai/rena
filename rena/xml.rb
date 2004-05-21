27ab77a8d7b0e7a7510b4bf4861f35c778a22125
Masahiro Sakai <masahiro.sakai@gmail.com>
2004-05-21 00:23:37 +0000

    git-svn-id: svn+ssh://kencho.tom.sfc.keio.ac.jp/home/sakai/svn/rena/trunk/rena@6 71edbfc0-72d9-0310-a89a-e1ccc01af32e

--- rena/xml.rb
+++ rena/xml.rb
@@ -17,17 +17,7 @@ class XMLReader
   end
 
   def read_from_xml_document(doc, base = URI.parse(""))
-    root = doc.root
-    unless root.is_a? REXML::Element
-      # FIXME
-      #raise ArgumentError
-    else
-      if root.namespace == RDF::Namespace and root.name == "RDF"
-	parse_rdf(root, base)
-      else
-	parse_resource(root, base)
-      end
-    end
+    parse_doc(doc, base)
   end
 
   private
@@ -59,25 +49,47 @@ class XMLReader
   XMLLiteral_DATATYPE_URI =
     URI.parse("http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral")
 
-  def parse_rdf(rdf, base, lang = nil)
-    base = update_base(base, rdf)
-    lang = update_lang(lang, rdf)
-
-    rdf.elements.each{|e|
-      parse_resource(e, base)
-    }
+  def parse_doc(doc, base = URI.parse(""))
+    root = doc.root
+    unless root.is_a? REXML::Element
+      # FIXME
+      #raise ArgumentError
+    else
+      if root.namespace == RDF::Namespace and root.name == "RDF"
+	parse_rdf(root, base)
+      else
+	parse_nodeElement(root, base) # XXX
+      end
+    end
   end
 
-  def parse_resource(e, base, lang = nil)
+  def parse_rdf(rdf, base, lang = nil)
+    if rdf.namespace == RDF::Namespace and rdf.name == "RDF"
+      base = update_base(base, rdf)
+      lang = update_lang(lang, rdf)
+      
+      # nodeElementList
+      rdf.elements.each{|e|
+	parse_nodeElement(e, base)
+      }
+    else
+      # XXX
+      raise TypeError.new
+    end
+  end
+
+  def parse_nodeElement(e, base, lang = nil)
     base = update_base(base, e)
     lang = update_lang(lang, e)
 
-    if about = e.attribute("about", RDF::Namespace)
-      subject = @model.create_resource(base + about.value)
-    elsif id = e.attribute("ID", RDF::Namespace)
+    # FIXME: nodeElementURIs であることをチェック
+
+    if id = e.attribute("ID", RDF::Namespace)
       subject = @model.create_resource(base + ("#" + id.value))
     elsif nodeID = e.attribute("nodeID", RDF::Namespace)
       subject = lookup_nodeID(nodeID.value)
+    elsif about = e.attribute("about", RDF::Namespace)
+      subject = @model.create_resource(base + about.value)
     else
       subject = @model.create_resource
     end
@@ -89,25 +101,135 @@ class XMLReader
 
     e.attributes.each_attribute{|attr|
       if predicate = attr_as_predicate(e, attr)
-	# FIXMR
 	if predicate == RDF::Type
-	  object = @model.create_resource(URI.parse(attr.value))
-	else
-	  object = PlainLiteral.new(attr.value, lang)
+	  subject.add_property(predicate,
+			       @model.create_resource(URI.parse(attr.value)))
+	else # FIXME: propertyAttrであることをチェック?
+	  subject.add_property(predicate,
+			       PlainLiteral.new(attr.value, lang))
 	end
-	subject.add_property(predicate, object)
       end
     }
 
-    parse_predicates(subject, e.elements, base, lang)
+    parse_propertyEltList(subject, e.elements, base, lang)
 
     subject
   end
 
-  def parse_predicates(subject, elements, base, lang)
+  def parse_propertyEltList(subject, elements, base, lang)
     i = 0
 
     elements.each{|e|
+      # propertyElt
+      new_base = update_base(base, e)
+      new_lang = update_lang(lang, e)
+
+      if e.namespace==RDF::Namespace and e.name == "li"
+  	predicate = URI.parse(RDF::Namespace + "_#{i+=1}")
+      else
+  	predicate = URI.parse(e.namespace + e.name)
+      end
+
+      if parseType = e.attribute("parseType", RDF::Namespace)
+  	case parseType.value
+  	when "Resource" # parseTypeResourcePropertyElt
+  	  object = @model.create_resource
+  	  parse_propertyEltList(object, e.elements, new_base, new_lang)
+  	when "Collection" # parseTypeCollectionPropertyElt
+	  items = e.elements.map{|e2| parse_nodeElement(e2, new_base) }
+	  object = items.reverse.inject(@model.create_resource(RDF::Nil)){
+	    |result, item|
+	    @model.create_resource.
+	      add_property(RDF::First, item).
+	      add_property(RDF::Rest, result)
+	  }
+	#when "Literal" # parseTypeLiteralPropertyElt
+	else # parseTypeOtherPropertyElt
+	  # FIXME: Exclusive XML Canonicalization
+	  s = ''
+	  e.children.each{|child| child.write(s) }
+	  object = TypedLiteral.new(s, XMLLiteral_DATATYPE_URI)
+  	end
+  
+      elsif e.elements.size == 1
+	# resourcePropertyElt
+	object = parse_nodeElement(e.elements[1], new_base, new_lang)
+
+      elsif e.children.any?{|c| c.is_a? REXML::Text }
+	# literalPropertyElt
+	s = e.children.map{|c| c.value }.join('')
+	if attr_type = e.attribute("datatype", RDF::Namespace)
+	  object = TypedLiteral.new(s, new_base + attr_type.value)
+	else
+	  object = PlainLiteral.new(s, new_lang)
+	end
+
+      else
+	# emptyPropertyElt
+
+	# If there are no attributes or only the optional rdf:ID attribute i.
+	flag = true
+	e.attributes.each_attribute{|attr|
+	  if attr.prefix=='' and /^xml/ =~ attr.local_name
+	    next
+	  end
+	  if (ns = e.namespace(attr.prefix)) and
+	      ns == RDF::Namespace and attr.local_name == 'ID'
+	    next
+	  end
+	  flag = false
+	  break
+	}
+
+	if flag
+	  object = PlainLiteral.new('', new_lang)
+	else
+	  if resource = e.attribute("resource", RDF::Namespace)
+	    object = @model.create_resource(base + resource.value)
+	  elsif nodeID = e.attribute("nodeID", RDF::Namespace)
+	    object = lookup_nodeID(nodeID.value)	
+	  else
+	    object = @model.create_resource
+	  end
+	end
+
+      end
+
+      subject.add_property(predicate, object)
+
+      if id = e.attribute("ID", RDF::Namespace)
+	@model.create_resource(new_base + ("#" + id.value)).
+	  add_property(RDF::Type, @model.create_resource(RDF::Statement)).
+	  add_property(RDF::Subject, subject).
+	  add_property(RDF::Predicate, @model.create_resource(predicate)).
+	  add_property(RDF::Object, object)
+      end
+
+      # XXX
+      unless object.is_a?(Rena::Literal)
+	e.attributes.each_attribute{|attr|
+	  if predicate2 = attr_as_predicate(e, attr)
+	    subject2 = object
+	    # FIXMR
+	    if predicate == RDF::Type
+	      object2 = @model.create_resource(URI.parse(attr.value))
+	    else
+	      object2 = PlainLiteral.new(attr.value, new_lang)
+	    end
+	    subject2.add_property(predicate2, object2)
+	  end
+	}
+      end
+    }
+  end
+
+=begin
+  def parse_propertyEltList(subject, elements, base, lang)
+    i = 0
+
+    elements.each{|e|
+      # propertyElt
+
       new_base = update_base(base, e)
       new_lang = update_lang(lang, e)
 
@@ -118,35 +240,36 @@ class XMLReader
       end
   
       if resource = e.attribute("resource", RDF::Namespace)
+	# resourcePropertyElt
   	object = @model.create_resource(base + resource.value)
       elsif nodeID = e.attribute("nodeID", RDF::Namespace)
   	object = lookup_nodeID(nodeID.value)
       elsif parseType = e.attribute("parseType", RDF::Namespace)
   	case parseType.value
-  	when "Resource"
+  	when "Resource" # parseTypeResourcePropertyElt
   	  object = @model.create_resource
-  	  parse_predicates(object, e.elements, new_base, new_lang)
-  	when "Collection"
-	  items = e.elements.map{|e2| parse_resource(e2, new_base) }
+  	  parse_propertyEltList(object, e.elements, new_base, new_lang)
+  	when "Collection" # parseTypeCollectionPropertyElt
+	  items = e.elements.map{|e2| parse_nodeElement(e2, new_base) }
 	  object = items.reverse.inject(@model.create_resource(RDF::Nil)){|result,item|
 	    @model.create_resource.
 	      add_property(RDF::First, item).
 	      add_property(RDF::Rest, result)
 	  }
-	when "Literal"
+	#when "Literal" # parseTypeLiteralPropertyElt
+	else # parseTypeOtherPropertyElt
 	  s = ''
 	  e.children.each{|child| child.write(s) }
 	  object = TypedLiteral.new(s, XMLLiteral_DATATYPE_URI)
-  	else
-  	  raise RuntimeError.new
   	end
       else
 	if e.elements.size == 1
-  	  object = parse_resource(e.elements[1], new_base)
+	  # resourcePropertyElt
+  	  object = parse_nodeElement(e.elements[1], new_base, new_lang)
 	elsif e.elements.size == 0
 	  # FIXME
 	  if e.children.any?{|child|
-	      child.is_a?(REXML::Text) and /\A\s*\Z/ !~ child.value
+	      child.is_a?(REXML::Text) and /\A\s*\Z/u !~ child.value
 	    }
 	    flag_blank = false
 	  else
@@ -162,6 +285,8 @@ class XMLReader
 	  if flag_blank
 	    object = @model.create_resource
 	  else
+	    # literalPropertyElt
+	    # emptyPropertyElt
 	    s = e.children.map{|child|
 	      child.is_a?(REXML::Text) ? child.value : ""
 	    }.join("")
@@ -204,11 +329,22 @@ class XMLReader
       end
     }
   end
+=end
 
   private
   def lookup_nodeID(nodeID)
     @blank_nodes[nodeID] ||= @model.create_resource
   end
+
+=begin
+  CoreSyntaxTerms_local_name = [
+    "RDF", "ID", "about", "parseType", "resource", "nodeID", "datatype"
+  ]
+  SyntaxTerms_local_name = CoreSyntaxTerms_local_name + [
+    "Description", "li"
+  ]
+  OldTerms_local_name = ["aboutEach", "aboutEachPrefix", "bagID"]
+=end
 
   RDF_SyntaxNames = ['RDF', 'Description', 'ID', 'about', 'parseType', 'resource', 'li', 'nodeID', 'datatype']
 
@@ -305,7 +441,7 @@ class XMLWriter
       next if prop == RDF::Type and object == type
 
       ename = fold_uri(prop)
-      #ename = "rdf:li" if /rdf:_\d+/ =~ ename
+      #ename = "rdf:li" if /rdf:_\d+/u =~ ename
 
       if first
 	first = false
@@ -338,7 +474,7 @@ class XMLWriter
   def fold_uri(uri)
     uri_s = uri.to_s
     @namespaces.each_pair{|prefix, namespace|
-      if /\A#{Regexp.quote(namespace)}(.*)\Z/ =~ uri_s
+      if /\A#{Regexp.quote(namespace)}(.*)\Z/u =~ uri_s
 	if prefix.empty?
 	  return $1
 	else
