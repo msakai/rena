658ccbe56b073f24227771ea4a18b83b63482d54
Masahiro Sakai <masahiro.sakai@gmail.com>
2004-05-26 07:02:50 +0000

    * make 'lib/' directory.
    git-svn-id: svn+ssh://kencho.tom.sfc.keio.ac.jp/home/sakai/svn/rena/trunk/rena@28 71edbfc0-72d9-0310-a89a-e1ccc01af32e

--- tests/test-xml-writer.rb
+++ tests/test-xml-writer.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 $KCODE='utf-8'
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..")
+$LOAD_PATH.unshift File.join(File.dirname(__FILE__), "../lib")
 require 'rena'
 require 'rena-test-utils'
 
